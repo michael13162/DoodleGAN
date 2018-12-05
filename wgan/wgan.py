74f27653e8a60b6b3880bb29084899f63f16d5a6
johnnyle24 <johnny.le24@gmail.com>
2018-12-05 02:15:25 -0700

    Added folder structure and additional files to .gitignore

--- wgan/wgan.py
+++ wgan/wgan.py
@@ -240,7 +240,7 @@ class WGANGP():
                 axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/mnist_%d.png" % epoch)
+        fig.savefig("data/images/mnist_%d.png" % epoch)
         plt.close()
 
 
